edcb008cc581d989f0613289703b1fd1d91c0d79
jetryan4 <jetryan4@vt.edu>
2021-08-01 19:45:20 -0400

    guess number solidity file

---
+++ guessNumber.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.1;
+contract guess_number{
+
+    uint secretNumber;
+
+    enum State {ACTIVE, COMPLETE}
+    State public currState;
+    uint balance;
+
+    contructor (uint _secretNumber) payable {
+        require(msg.value >= 10*10**18, 'this contract needs to be funded with 10 Eth');
+        secretNumber = _secretNumber;
+        balance = msg.value;
+    }
+
+    function getBalance() public view returns (uint) {
+        return balance;
+    }
+
+    function play(address payable player, uint _numberGuess) external payable returns (uint) {
+        require(msg.value >= 10**18, 'pay at least one Eth to gain');
+        require(currState == State.ACTIVE, 'Too late');
+        balance = balance + msg.value;
+        if (_numberGuess == secretNumber){
+            player.transfer(address(this).balance);
+            currState.State.COMPLETE;
+            return balance;
+        }
+        else{
+            return balance;
+        }
+    }
+
+}
